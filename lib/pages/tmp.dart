23392f1f2e7cc49024e30156dc72cdf916b14246
Артем Пучков <aspuchkov02@gmail.com>
2022-09-27 14:17:20 +0300

    test

---
+++ lib/pages/tmp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/src/widgets/container.dart';
+
+import 'package:flutter/src/foundation/key.dart';
+
+import 'account_page.dart';
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
